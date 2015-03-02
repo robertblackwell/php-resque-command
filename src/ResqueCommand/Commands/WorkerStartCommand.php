b7f632524410c922746329f6c2e7eb7286100b53
ROBERT <rob@whiteacorn.com>
2015-03-02 15:31:51 -0700

    some cleanup of the logging

--- src/ResqueCommand/Commands/WorkerStartCommand.php
+++ src/ResqueCommand/Commands/WorkerStartCommand.php
@@ -28,10 +28,10 @@ class WorkerStartCommand extends WorkerBaseCommand
 							InputOption::VALUE_OPTIONAL, 'The host id for the redis server ip:port', '127.0.0.1:6379'),
                 
 				new InputOption('redis_index', "redis_index|-x|--redis_index", InputOption::VALUE_OPTIONAL, 'database index to use for redis', '9'),
-                new InputOption('logging', "logging|-l|--logging", InputOption::VALUE_OPTIONAL, 'is loggin on', '0'),
                 new InputOption('queue', "queue|-u|--queue", InputOption::VALUE_OPTIONAL, ' queue name', ''),
-                new InputOption('interval', "interval|-i|--interval", InputOption::VALUE_OPTIONAL, ' queue name', ''),
-                new InputOption('blocking', "blocking|-b|--blocking", InputOption::VALUE_OPTIONAL, ' is blocking set', true),
+                new InputOption('interval', "interval|-i|--interval", InputOption::VALUE_OPTIONAL, ' polling interval', ''),
+				new InputOption('logging', "logging|-l|--logging", InputOption::VALUE_NONE, 'is logging on'),
+                new InputOption('blocking', "blocking|-b|--blocking", InputOption::VALUE_NONE, ' is blocking set'),
 			))
             ->setHelp("<info>%command.name%</info> command starts a single php-resque worker process.");
     }
@@ -41,13 +41,15 @@ class WorkerStartCommand extends WorkerBaseCommand
 			'app_include' => ($app_include = $input->getOption('app_include')),
 	        'redis_server' => ( $redis_server = $input->getOption('redis_server')),
 	        'redis_index' => ( $redis_index = $input->getOption('redis_index')),
-	        'logging' => ($logging =  $input->hasOption('logging')),
+	        'logging' => ($logging =  $input->getOption('logging')),
+			'loggingValue'=> $input->getOption('logging'),
+			'options' => $input->getOptions(),
 	        'queue' => ( $queue = $input->getOption('queue')),
 	        'interval' => ( $interval = $input->getOption('interval')),
-	        'blocking' => ( $blocking = $input->hasOption('blocking')),
+	        'blocking' => ( $blocking = $input->getOption('blocking')),
 		];
 
-		$output->writeln(print_r($args, true));
+//		$output->writeln(print_r($args, true));
 
 		\ResqueCommand\ResqueWrapper::startWorker(
 			$app_include,
--- src/ResqueCommand/ResqueWrapper.php
+++ src/ResqueCommand/ResqueWrapper.php
@@ -8,12 +8,13 @@ class ResqueWrapper
 		    $redisServer,   // in the form "127.0.0.1:9367
 		    $redisDatabase, // database index eg, 9
 		    $queue,         // string
-		    $logLevel=true,
+		    $logLevel=false, //turns on/off INFO DEBUG messages only - more important are always ON
 		    $interval=5,
 		    $blocking=true
 		)
 		{
 			if( is_string($app_requires)) $app_requires = [$app_requires];
+			
 			if( is_array($app_requires) ){
 			    foreach($app_requires as $file) {
 			        if( ! file_exists($file) ){
@@ -25,13 +26,6 @@ class ResqueWrapper
 		    $logger = new \Resque_Log($logLevel);
 
 		    \Resque::setBackend($redisServer, $redisDatabase);
-
-		    $logger->log(
-		        \Psr\Log\LogLevel::NOTICE,
-		        "Starting background processor queue(  {$queue} ) interval: {$interval} (seconds)  ",
-		        ['queue' => $queue, 'interval' => $interval]
-		    );
-
 
 		    $worker = new \Resque_Worker([$queue]);
 		    $worker->setLogger($logger);
